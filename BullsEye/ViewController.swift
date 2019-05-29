6f9a43b724a6733ab541a433d1c00a7e04b7f448
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-05-29 16:35:09 -0400

    updated the value of the slider

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  var currentValue: Int = 0
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
+  
+  @IBAction func showAlert() {
+    
+    let message = "the value of the slider is now: \(currentValue)"
 
+    let alert = UIAlertController(title: "Hello World!", message: message, preferredStyle: .alert)
+    
+    let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+    
+    alert.addAction(action)
+    
+    present(alert, animated: true, completion: nil)
+    
+  }
+  
+  @IBAction func slider(_ slider: UISlider) {
+    let roundedValue = slider.value.rounded()
+    currentValue = Int(slider.value)
+  }
 
 }
 
