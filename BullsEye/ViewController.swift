6ed651c905cd6f776e1402feffd232fe15d51511
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-07-03 14:44:35 -0400

    refined the algo to function faster

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -27,14 +27,9 @@ class ViewController: UIViewController {
   
   @IBAction func showAlert() {
     
-    var difference : Int
-    
-    if currentValue > targetValue {
-      difference = currentValue - targetValue
-    } else if targetValue > currentValue {
-      difference = targetValue - currentValue
-    } else {
-      difference = 0
+    var difference = currentValue - targetValue
+    if difference < 0 {
+      difference = difference * -1
     }
     
     let message = "the value of the slider is now: \(currentValue)"
