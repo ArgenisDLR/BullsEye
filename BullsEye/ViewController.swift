92eb2b8c1e2d707df7f8862be379251c00a97e28
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-07-03 12:29:01 -0400

    updated several methods

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,10 +11,15 @@ import UIKit
 class ViewController: UIViewController {
   
   var currentValue: Int = 0
+  var targetValue: Int = 0
+  
+  @IBOutlet weak var slider: UISlider!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    currentValue = Int()
+    targetValue = Int.random(in: 1...100)
   }
   
   @IBAction func showAlert() {
@@ -34,6 +39,13 @@ class ViewController: UIViewController {
   @IBAction func slider(_ slider: UISlider) {
     let roundedValue = slider.value.rounded()
     currentValue = Int(slider.value)
+  }
+  
+  func startNewRound() {
+    
+    targetValue = Int.random(in: 1...100)
+    currentValue = 50
+    slider.value = Float(currentValue)
   }
 
 }
